4c23bb705ed1290590cc9b583cda52f31ba27353
anntnzrb <51257127+anntnzrb@users.noreply.github.com>
2025-10-08 05:58:22 +0000

    feat(web): add actor management forms

--- BackendPro.Web/Controllers/ActoresController.cs
+++ BackendPro.Web/Controllers/ActoresController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BackendPro.Core.DTOs;
+using BackendPro.Core.Entities;
 using BackendPro.Infrastructure.Data;
 using BackendPro.Web.Models;
 
@@ -11,6 +12,7 @@ public class ActoresController(ApplicationDbContext context) : Controller
     private readonly ApplicationDbContext _context = context;
 
     // GET: Actores
+    [HttpGet]
     public async Task<IActionResult> Index()
     {
         var actores = await _context.Actores.ToListAsync().ConfigureAwait(false);
@@ -27,6 +29,7 @@ public class ActoresController(ApplicationDbContext context) : Controller
     }
 
     // GET: Actores/Details/5
+    [HttpGet]
     public async Task<IActionResult> Details(int? id)
     {
         if (id == null)
@@ -74,5 +77,145 @@ public class ActoresController(ApplicationDbContext context) : Controller
         };
 
         return View(viewModel);
+    }
+
+    // GET: Actores/Create
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View(new ActorFormViewModel
+        {
+            FechaNacimiento = DateTime.Today,
+        });
+    }
+
+    // POST: Actores/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(ActorFormViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        var actor = new Actor
+        {
+            Nombre = viewModel.Nombre,
+            Biografia = viewModel.Biografia,
+            FechaNacimiento = viewModel.FechaNacimiento,
+        };
+
+        _context.Actores.Add(actor);
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Actores/Edit/5
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var actor = await _context.Actores
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id).ConfigureAwait(false);
+
+        if (actor == null)
+        {
+            return NotFound();
+        }
+
+        var viewModel = new ActorFormViewModel
+        {
+            Id = actor.Id,
+            Nombre = actor.Nombre,
+            Biografia = actor.Biografia,
+            FechaNacimiento = actor.FechaNacimiento,
+        };
+
+        return View(viewModel);
+    }
+
+    // POST: Actores/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, ActorFormViewModel viewModel)
+    {
+        if (id != viewModel.Id)
+        {
+            return BadRequest();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        var actor = await _context.Actores
+            .FirstOrDefaultAsync(a => a.Id == id).ConfigureAwait(false);
+
+        if (actor == null)
+        {
+            return NotFound();
+        }
+
+        actor.Nombre = viewModel.Nombre;
+        actor.Biografia = viewModel.Biografia;
+        actor.FechaNacimiento = viewModel.FechaNacimiento;
+
+        await _context.SaveChangesAsync().ConfigureAwait(false);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Actores/Delete/5
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var actor = await _context.Actores
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id).ConfigureAwait(false);
+
+        if (actor == null)
+        {
+            return NotFound();
+        }
+
+        var viewModel = new ActorDto
+        {
+            Id = actor.Id,
+            Nombre = actor.Nombre,
+            Biografia = actor.Biografia,
+            FechaNacimiento = actor.FechaNacimiento,
+        };
+
+        return View(viewModel);
+    }
+
+    // POST: Actores/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var actor = await _context.Actores
+            .FirstOrDefaultAsync(a => a.Id == id).ConfigureAwait(false);
+
+        if (actor != null)
+        {
+            _context.Actores.Remove(actor);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        return RedirectToAction(nameof(Index));
     }
 }
