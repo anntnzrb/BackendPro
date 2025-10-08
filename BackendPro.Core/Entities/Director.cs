e20574434997d02c506ed92997c69d60ab56a8d9
anntnzrb <51257127+anntnzrb@users.noreply.github.com>
2025-10-08 05:24:07 +0000

    style(core): adopt collection expressions in entities

--- BackendPro.Core/Entities/Director.cs
+++ BackendPro.Core/Entities/Director.cs
@@ -3,12 +3,12 @@ namespace BackendPro.Core.Entities;
 public class Director
 {
     public int Id { get; set; }
-    
+
     public string Nombre { get; set; } = string.Empty;
-    
+
     public string Nacionalidad { get; set; } = string.Empty;
-    
+
     public DateTime FechaNacimiento { get; set; }
-    
-    public virtual ICollection<Pelicula> Peliculas { get; set; } = new List<Pelicula>();
+
+    public virtual ICollection<Pelicula> Peliculas { get; set; } = [];
 }
